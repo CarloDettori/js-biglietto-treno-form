4529124a7eb50f54f2380e7ce8ddad44fd992072
CarloDettori <dettoric07@gmail.com>
2024-11-08 21:01:11 +0100

    deleting allert and prompt from javascript

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 "use strict"
 console.clear();
 //- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-let km = prompt("inserisci in km la distanza che desideri percorrere");
+let km = 0;
 let discount = "";
 
 
 //- va applicato uno sconto del 20% per i minorenni
 //- va applicato uno sconto del 40% per gli over 65.
-let age = prompt("Inserisci qui la tua età (in numero)")
+let age = null
 
 //- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
@@ -16,11 +16,14 @@ if (age > 65) {
 } else if (age >= 18) {
     discount = (km * 0.21) / 100 * 20;
 } else if (age < 0) {
-    console.log("eta inserita non valida, ricarica la pagina e riprova")
+    alert("eta inserita non valida, ricarica la pagina e riprova")
 } else {
     discount = 0;
 }
 
+// calcolo del prezzodele biglietto
 const price = km * 0.21 - discount;
-let priceForHumans = alert("Il prezzo del tuo viaggio è " + Math.round(price * 100) / 100 + "€");
+let priceForHumans = Math.round(price * 100) / 100 + "€";
 console.log("Il prezzo del tuo viaggio è " + Math.round(price * 100) / 100 + "€");
+
+let generateTrigger = 
